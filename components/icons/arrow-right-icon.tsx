6a223b3ba7310fd1babbb156dc7407af11bfb5c7
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2023-05-30 17:55:11 -0400

    init icons

---
+++ components/icons/arrow-right-icon.tsx
@@ -0,0 +1,19 @@
+export default function ArrowRightIcon({ className }: { className: string }) {
+  return (
+    <svg
+      fill="none"
+      height="24"
+      shapeRendering="geometricPrecision"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      width="24"
+      className={className}
+    >
+      <path d="M5 12h14" />
+      <path d="M12 5l7 7-7 7" />
+    </svg>
+  );
+}
