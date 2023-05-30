8451ef4d5c149ce34e9b641b04af5ae5e7641810
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2023-05-30 18:00:14 -0400

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <main>Hello world</main>;
+import CafeCard from "@/components/cafe-card";
+import { MOCK_DATA } from "@/lib/mock-data";
+
+export default async function Home() {
+  return (
+    <main>
+      <h1>Hello World</h1>
+      {MOCK_DATA.map((cafe) => {
+        return <CafeCard key={cafe.place_id} cafe={cafe} />;
+      })}
+    </main>
+  );
 }
