728479e970365b6dd7b764859fb07ba370a6e984
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2023-05-30 17:58:24 -0400

    init mock data

---
+++ lib/mock-data.ts
@@ -0,0 +1,105 @@
+export const MOCK_DATA = [
+  {
+    place_id: "ChIJ_zD6ESDGwoAR-wg2QUIAySo",
+    latitude: 34.0309478,
+    longitude: -118.2301719,
+    name: "Maru Coffee",
+    updatedAt: "2023-05-18T15:57:55.654Z",
+    reviews: [
+      {
+        id: 45,
+        email: "kevinmitch14@gmail.com",
+        place_id: "ChIJ_zD6ESDGwoAR-wg2QUIAySo",
+        rating: 4,
+      },
+    ],
+  },
+
+  {
+    place_id: "ChIJnT4WyQTzQA0RRZGE7j8DGFs",
+    latitude: 40.6546318,
+    longitude: -4.6962929,
+    name: "Santa Teresa Shop & Café",
+    updatedAt: "2023-03-12T22:19:31.429Z",
+    reviews: [
+      {
+        id: 38,
+        email: "kevinmitch14@gmail.com",
+        place_id: "ChIJnT4WyQTzQA0RRZGE7j8DGFs",
+        rating: 4,
+      },
+    ],
+  },
+  {
+    place_id: "ChIJbdsh9ncFdkgRwiyMWFs_usE",
+    latitude: 51.48651479999999,
+    longitude: -0.1708343,
+    name: "Peggy Porschen Chelsea",
+    updatedAt: "2023-03-12T22:16:01.765Z",
+    reviews: [
+      {
+        id: 36,
+        email: "kevinmitch14@gmail.com",
+        place_id: "ChIJbdsh9ncFdkgRwiyMWFs_usE",
+        rating: 5,
+      },
+      {
+        id: 37,
+        email: "kevinmitch14@gmail.com",
+        place_id: "ChIJbdsh9ncFdkgRwiyMWFs_usE",
+        rating: 5,
+      },
+    ],
+  },
+  {
+    place_id: "ChIJfyNSrpkb2jERuDCdA58fvTc",
+    latitude: 1.3225821,
+    longitude: 103.8140517,
+    name: "PRAIRIE by Craftsmen (Bukit Timah)",
+    updatedAt: "2023-02-08T19:59:08.535Z",
+    reviews: [
+      {
+        id: 4,
+        email: "kevinmitch14@gmail.com",
+        place_id: "ChIJfyNSrpkb2jERuDCdA58fvTc",
+        rating: 5,
+      },
+    ],
+  },
+  {
+    place_id: "ChIJ98Er0TGRW0gReN0fnKQRzv0",
+    latitude: 53.2906203,
+    longitude: -8.9865314,
+    name: "Grind Briarhill",
+    updatedAt: "2023-02-08T19:38:59.037Z",
+    reviews: [
+      {
+        id: 1,
+        email: "kevinmitch14@gmail.com",
+        place_id: "ChIJ98Er0TGRW0gReN0fnKQRzv0",
+        rating: 5,
+      },
+      {
+        id: 3,
+        email: "kevinmitch14@gmail.com",
+        place_id: "ChIJ98Er0TGRW0gReN0fnKQRzv0",
+        rating: 5,
+      },
+    ],
+  },
+  {
+    place_id: "ChIJx-HsgpHxby4RBXpB9qy_DWg",
+    latitude: -7.228727999999999,
+    longitude: 109.2867026,
+    name: "Pratin",
+    updatedAt: "2023-02-08T19:38:50.306Z",
+    reviews: [
+      {
+        id: 2,
+        email: "kevinmitch14@gmail.com",
+        place_id: "ChIJx-HsgpHxby4RBXpB9qy_DWg",
+        rating: 4,
+      },
+    ],
+  },
+];
