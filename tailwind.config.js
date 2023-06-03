2d417d2dc673f8de8fdf2023254709479a321949
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2023-06-03 14:05:58 -0400

    add enter-up animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,10 +63,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "enter-up": {
+          from: { opacity: "0", transform: "translateY(16px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "enter-up": "enter-up 0.2s ease-out"
       },
     },
   },
