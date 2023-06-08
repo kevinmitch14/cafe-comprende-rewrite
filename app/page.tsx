4efa36e59e0827a34f7f5e9cc82354be201f881f
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2023-06-07 23:05:02 -0400

    feat(data): fetch data from DB and use in app

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import CafeCard from "@/components/cafe-card";
+import { db } from "@/lib/db";
+import { cafe } from "@/lib/db/schema";
 import { MOCK_DATA } from "@/lib/mock-data";
+import { desc } from "drizzle-orm";
 
-export default function Home() {
+export default async function Home() {
+  const data = await db.query.cafe.findMany({
+    with: {
+      review: true,
+    },
+    orderBy: desc(cafe.updatedAt),
+  });
   return (
     <main>
       <h1>Hello World</h1>
-      {MOCK_DATA.map((cafe) => {
-        return <CafeCard key={cafe.place_id} cafe={cafe} />;
+      {data.map((cafe) => {
+        return <CafeCard key={cafe.placeId} cafe={cafe} />;
       })}
     </main>
   );
