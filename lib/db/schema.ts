1109d890d10c1abba5e1dc1568b7de2f61977402
Kevin Mitchell <kevinmitch14@gmail.com>
2023-07-30 21:55:19 -0400

    fix: typo

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -45,7 +45,7 @@ export const cafe = mysqlTable(
     longitude: double("longitude").notNull(),
     name: varchar("name", { length: 191 }).notNull(),
     updatedAt: datetime("updatedAt", { mode: "string", fsp: 3 }).notNull(),
-    country: varchar("country", {
+    country: varchar("country_code", {
       length: 2,
     })
       .notNull()
