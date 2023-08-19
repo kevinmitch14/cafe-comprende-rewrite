bd1f7f1dd4d7c26b666b644fb777d8ac6c8eca2a
Kevin Mitchell <kevinmitch14@gmail.com>
2023-08-18 20:41:53 -0400

    feat: loading spinner + loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingSpinner from "@/components/loading-spinner";
+
+export default function Loading() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <LoadingSpinner className="h-5 w-5" />
+    </main>
+  );
+}
