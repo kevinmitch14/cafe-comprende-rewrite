f04944f7db76c890a98144e809b63a79ba98cf80
Kevin Mitchell <kevinmitch14@gmail.com>
2023-08-18 21:05:50 -0400

    feat: add messsage if no cafes in selected country

--- components/cafe-list.tsx
+++ components/cafe-list.tsx
@@ -1,8 +1,15 @@
 import { getCafes } from "@/lib/getCafes";
 import CafeCard from "./cafe-card";
+import { countryCodeToName } from "@/lib/countries";
 
 export default async function CafeList({ country }: { country: string }) {
   const cafes = await getCafes(country);
-
+  if (cafes.length === 0) {
+    return (
+      <p className="mx-auto p-4 font-medium">
+        {`No cafes in ${countryCodeToName.get(country)}.`}
+      </p>
+    );
+  }
   return cafes.map((cafe) => <CafeCard cafe={cafe} key={cafe.placeId} />);
 }
