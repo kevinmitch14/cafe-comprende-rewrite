b13e9d1ebd2d72adba7963899ab4ead765c1f4c5
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2024-02-19 07:38:39 -0300

    Vaul integration (#8)
    * feat(WIP): `<Drawer />` from `vaul`
    
    * add images
    
    * new sizes
    
    * more WIP
    
    * modal={!false}
    
    * wip
    
    * remove scroll
    
    * update zustand types + structure
    
    * more wip vaul

--- components/mapbox.tsx
+++ components/mapbox.tsx
@@ -1,19 +1,31 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useRef } from "react";
-import { Cross2Icon, ExternalLinkIcon } from "@radix-ui/react-icons";
+import {
+  Cross2Icon,
+  PlusCircledIcon,
+  StarFilledIcon,
+} from "@radix-ui/react-icons";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { RemoveScroll } from "react-remove-scroll";
 import { GetCafes } from "@/components/cafe-list";
-import { Badge } from "@/components/ui/badge";
 import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-} from "@/components/ui/sheet";
-import { useCafeStore } from "@/lib/store/cafe-store";
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerOverlay,
+  DrawerPortal,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { type BearState, useCafeStore } from "@/lib/store/cafe-store";
+import { Button } from "./ui/button";
+
+const sampleImages = [
+  "https://images.unsplash.com/photo-1567880905822-56f8e06fe630?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1559305616-3f99cd43e353?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FmZXxlbnwwfHwwfHx8MA%3D%3D",
+];
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_API_KEY!;
 
@@ -93,7 +105,7 @@ export function MapBox({ cafeData }: { cafeData: GetCafes[] }) {
               : 6,
           center: { lat: cafe.latitude, lng: cafe.longitude },
         });
-        setSelectedCafe(cafe);
+        setSelectedCafe({ ...cafe, type: "rated" });
         setLatitude(cafe.latitude);
         setLongitude(cafe.longitude);
       });
@@ -121,58 +133,105 @@ export function MapBox({ cafeData }: { cafeData: GetCafes[] }) {
       <div
         className="relative inset-0 h-full w-full rounded-md"
         ref={mapContainer}
-      >
-        <div className="absolute bottom-0 w-full">
-          <Sheet
-            modal={false}
-            open={!!selectedCafe && "address_components" in selectedCafe}
-          >
-            <SheetContent
-              className="absolute rounded-t-lg border-2"
-              side={"bottom"}
-            >
-              <SheetHeader>
-                <SheetTitle className="flex items-center gap-3">
-                  {selectedCafe?.name}
-                </SheetTitle>
-                <div className="flex gap-x-1">
-                  <a
-                    // TODO cleanup this TS mess :(
-                    href={`https://www.google.com/maps/dir/?api=1&destination_place_id=${
-                      selectedCafe && "place_id" in selectedCafe
-                        ? selectedCafe?.place_id
-                        : selectedCafe && "placeId" in selectedCafe
-                        ? selectedCafe?.placeId
-                        : null
-                    }&destination=${
-                      selectedCafe && "place_id" in selectedCafe
-                        ? selectedCafe?.place_id
-                        : selectedCafe && "placeId" in selectedCafe
-                        ? selectedCafe?.placeId
-                        : null
-                    }`}
-                  >
-                    <Badge
-                      className="flex items-center gap-x-1"
-                      variant={"secondary"}
-                    >
-                      Directions
-                      <ExternalLinkIcon className="h-3 w-3 text-blue-500" />
-                    </Badge>
-                  </a>
-                </div>
-                <SheetClose
-                  onClick={() => setSelectedCafe(null)}
-                  className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
-                >
-                  <Cross2Icon className="h-4 w-4" />
-                  <span className="sr-only">Close</span>
-                </SheetClose>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
-        </div>
-      </div>
+      />
+      <CafeDrawer selectedCafe={selectedCafe} />
     </div>
+  );
+}
+
+function CafeDrawer({
+  selectedCafe,
+}: {
+  selectedCafe: BearState["selectedCafe"];
+}) {
+  const { setSelectedCafe } = useCafeStore();
+
+  if (!selectedCafe) return;
+  return (
+    <Drawer
+      modal={false}
+      open={!!selectedCafe}
+      onClose={() => setSelectedCafe(null)}
+      snapPoints={[0.4, 0.9]}
+    >
+      <DrawerPortal>
+        <DrawerContent className="mx-[-1px] h-full max-h-[97%] flex-col border bg-background md:hidden">
+          <div className={"mx-auto w-full max-w-md space-y-3 p-4 pt-5"}>
+            <DrawerTitle>{selectedCafe.name}</DrawerTitle>
+            {/* https://github.com/emilkowalski/vaul/issues/168 */}
+            <CafeDrawerContent cafe={selectedCafe} />
+          </div>
+          <DrawerClose
+            onClick={() => setSelectedCafe(null)}
+            className="absolute right-4 top-4 inline-flex h-6 w-6 items-center justify-center rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
+          >
+            <Cross2Icon className="h-4 w-4" />
+            <span className="sr-only">Close</span>
+          </DrawerClose>
+        </DrawerContent>
+      </DrawerPortal>
+      <DrawerOverlay />
+    </Drawer>
+  );
+}
+
+function CafeDrawerContent({
+  cafe,
+}: {
+  cafe: NonNullable<BearState["selectedCafe"]>;
+}) {
+  if (cafe.type === "google")
+    return (
+      <>
+        <div className="flex items-center gap-x-1 text-sm font-medium text-muted-foreground">
+          <StarFilledIcon />
+          {cafe.rating}
+        </div>
+        {cafe.photos && (
+          <RemoveScroll className="flex snap-x snap-mandatory items-center gap-x-2 overflow-x-auto">
+            {cafe.photos.map((photo) => {
+              const url = photo.getUrl();
+              return (
+                <img
+                  width={150}
+                  height={150}
+                  src={url}
+                  alt="Image"
+                  key={url}
+                  className="aspect-square snap-center snap-always rounded-lg"
+                />
+              );
+            })}
+          </RemoveScroll>
+        )}{" "}
+      </>
+    );
+  return (
+    <>
+      <div className="flex items-center gap-x-1 text-sm font-medium text-muted-foreground">
+        <StarFilledIcon />
+        {cafe.averageRating.toFixed(1)}
+      </div>
+      <RemoveScroll className="flex snap-x snap-mandatory items-center gap-x-2 overflow-x-auto">
+        {[...sampleImages, ...sampleImages].map((link, idx) => (
+          <Image
+            key={idx}
+            width={150}
+            height={150}
+            className="aspect-square snap-center snap-always rounded-lg"
+            src={link}
+            alt={`Image ${idx}`}
+          />
+        ))}
+        <Button asChild className="h-auto" variant={"secondary"}>
+          <div className="flex min-w-[150px] snap-center snap-always flex-col items-center justify-center gap-2 self-stretch rounded-lg bg-muted">
+            <PlusCircledIcon className="h-5 w-5 text-muted-foreground" />
+            <span className="text-xs font-medium text-muted-foreground">
+              Add photo
+            </span>
+          </div>
+        </Button>
+      </RemoveScroll>
+    </>
   );
 }
