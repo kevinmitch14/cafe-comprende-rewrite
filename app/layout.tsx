f23dd579ab12f46e27572b75a1581354bcb2ca2a
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2024-03-11 10:05:34 -0300

    feat: add `vercel analytics`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { NavBar } from "@/components/nav-bar";
 import { Toaster } from "@/components/ui/toast";
@@ -26,6 +27,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={`${inter.className} antialiased`}>
           <SpeedInsights />
+          <Analytics />
           <Toaster />
           <NavBar />
           {children}
